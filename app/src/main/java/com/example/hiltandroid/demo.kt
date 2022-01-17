70bc5b7cb832bfedb572aca93ceb039699b817fd
lakshu <ced15i004@iiitdm.ac.in>
2022-01-17 15:08:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hiltandroid/demo.kt
@@ -0,0 +1,7 @@
+package com.example.hiltandroid
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class demo:Application()
---
+++ app/src/main/java/com/example/hiltandroid/hiltmodules.kt
@@ -0,0 +1,11 @@
+package com.example.hiltandroid
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+
+@Module
+@InstallIn(ActivityComponent::class)
+object values{
+    
+}
